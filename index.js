1e16e4b744a2f5384897fc67e415ad7e11e7da42
michael <michael@joinbox.com>
2012-12-30 16:12:38 +0100

    first super simple version

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+	var http = require( "http" );
+
+
+	var port = 80;
+	process.argv.forEach( function( arg ){
+		if ( reg = /--port=([0-9]{1,5})/gi.exec( arg ) ){
+			port = parseInt( reg[ 1 ], 10 );
+		}
+	} );
+
+
+	var server = http.createServer( function( request, response ){
+		response.writeHead( 301, { location: "https://" + request.headers.host + request.url } );
+		response.end();
+	} );
+
+	server.on( "listening", function(){
+		console.log( "server is listening on port", port );
+	} );
+
+	server.maxHeadersCount = 50;
+	server.listen( port );
